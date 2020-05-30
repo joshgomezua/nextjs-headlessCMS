fdca9fc23220b4941f56ec0b7751b214407de192
Nicolas Pineda <nicholas.pineda87@gmail.com>
2020-05-30 13:00:07 -0700

    Rule Menu Page Changed to Link Wrapper

--- src/pages/rules.js
+++ src/pages/rules.js
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import Link from '../components/Link/Link';
 import Layout from '../components/Layout';
 
 const Rules = () => {
@@ -8,75 +8,83 @@ const Rules = () => {
         Memorize The Most Tested Rules
       </div>
       <div className='flex flex-wrap mb-4 rounded justify-center cursor-pointer'>
-        {[
-          {
-            subject: `Agency and Partnership`,
-            url: `agency-and-partnership`,
-          },
-          {
-            subject: `Business Associations`,
-            url: `business-associations`,
-          },
-          {
-            subject: `Civil Procedure`,
-            url: `civil-procedure`,
-          },
-          {
-            subject: `Community Property`,
-            url: `community-property`,
-          },
-          {
-            subject: `Constitutional Law`,
-            url: `constitutional-law`,
-          },
-          {
-            subject: `Contracts`,
-            url: `contracts`,
-          },
-          {
-            subject: `Criminal Law`,
-            url: `criminal-law`,
-          },
-          {
-            subject: `Criminal Procedure`,
-            url: `criminal-procedure`,
-          },
-          {
-            subject: `Evidence`,
-            url: `evidence`,
-          },
-          {
-            subject: `Professional Responsibility`,
-            url: `professional-responsibility`,
-          },
-          {
-            subject: `Real Property`,
-            url: `real-property`,
-          },
-          {
-            subject: `Remedies`,
-            url: `remedies`,
-          },
-          {
-            subject: `Torts`,
-            url: `torts`,
-          },
-          {
-            subject: `Wills and Trusts`,
-            url: `wills-and-trusts`,
-          },
-        ].map((essay) => (
-          <div className='max-w-sm shadow-md w-1/2' key={essay.subject}>
-            <Link href='/rules/url' as={`/rules/${essay.url}`}>
-              <div className='px-4 py-4 hover:bg-red-700 hover:text-white rounded-md'>
-                <div className='font-bold text-2xl'>{essay.subject}</div>
+        <div className='max-w-sm shadow-md w-1/2'>
+            <Link href='/rules/agency-and-partnership'>
+              <div className='px-4 py-4 hover:bg-red-700 hover:text-white rounded-md shadow-md'>
+                <div className='font-bold text-2xl'>Agency and Partnership</div>
               </div>
             </Link>
-          </div>
-        ))}
+            <Link href='/rules/business-associations'>
+              <div className='px-4 py-4 hover:bg-red-700 hover:text-white rounded-md shadow-md'>
+                <div className='font-bold text-2xl'>Business Associations</div>
+              </div>
+            </Link>
+            <Link href='/rules/civil-procedure'>
+              <div className='px-4 py-4 hover:bg-red-700 hover:text-white shadow-md rounded-md'>
+                <div className='font-bold text-2xl'>Civil Procedure</div>
+              </div>
+            </Link>
+            <Link href='/rules/community-property'>
+              <div className='px-4 py-4 hover:bg-red-700 hover:text-white shadow-md rounded-md'>
+                <div className='font-bold text-2xl'>Community Property</div>
+              </div>
+            </Link>
+            <Link href='/rules/constitutional-law'>
+              <div className='px-4 py-4 hover:bg-red-700 hover:text-white shadow-md rounded-md'>
+                <div className='font-bold text-2xl'>Constitutional Law</div>
+              </div>
+            </Link>
+            <Link href='/rules/contracts'>
+              <div className='px-4 py-4 hover:bg-red-700 hover:text-white shadow-md rounded-md'>
+                <div className='font-bold text-2xl'>Contracts</div>
+              </div>
+            </Link>
+            <Link href='/rules/criminal-law'>
+              <div className='px-4 py-4 hover:bg-red-700 hover:text-white shadow-md rounded-md'>
+                <div className='font-bold text-2xl'>Criminal Law</div>
+              </div>
+            </Link>
+        </div>
+        <div className='max-w-sm shadow-md w-1/2'>
+            <Link href='/rules/criminal-procedure'>
+              <div className='px-4 py-4 hover:bg-red-700 hover:text-white shadow-md rounded-md'>
+                <div className='font-bold text-2xl'>Criminal Procedure</div>
+              </div>
+            </Link>
+            <Link href='/rules/evidence'>
+              <div className='px-4 py-4 hover:bg-red-700 hover:text-white shadow-md rounded-md'>
+                <div className='font-bold text-2xl'>Evidence</div>
+              </div>
+            </Link>
+            <Link href='/rules/professional-responsibility'>
+              <div className='px-4 py-4 hover:bg-red-700 hover:text-white shadow-md rounded-md'>
+                <div className='font-bold text-2xl'>Professional Responsibility</div>
+              </div>
+            </Link>
+            <Link href='/rules/real-property'>
+              <div className='px-4 py-4 hover:bg-red-700 hover:text-white shadow-md rounded-md'>
+                <div className='font-bold text-2xl'>Real Property</div>
+              </div>
+            </Link>
+            <Link href='/rules/remedies'>
+              <div className='px-4 py-4 hover:bg-red-700 hover:text-white shadow-md rounded-md'>
+                <div className='font-bold text-2xl'>Remedies</div>
+              </div>
+            </Link>
+            <Link href='/rules/torts'>
+              <div className='px-4 py-4 hover:bg-red-700 hover:text-white shadow-md rounded-md'>
+                <div className='font-bold text-2xl'>Torts</div>
+              </div>
+            </Link>
+            <Link href='/rules/wills-and-trusts'>
+              <div className='px-4 py-4 hover:bg-red-700 hover:text-white shadow-md rounded-md'>
+                <div className='font-bold text-2xl'>Wills and Trusts</div>
+              </div>
+            </Link>
+        </div>
       </div>
     </Layout>
-  );
-};
+  )
+  }
 
 export default Rules;
--- src/pages/subjects/[id].js
+++ src/pages/subjects/[id].js
@@ -14,7 +14,7 @@ const Subjects = ({ subject, essays }) => {
           {essays
             .sort((a, b) => b.year - a.year)
           .map((essay, index) => (
-            <div className='max-w-sm  shadow-md w-1/2' key={index}>
+            <div className='max-w-sm shadow-md w-1/2' key={index}>
               <Link href='/essays/[id]' as={`/essays/${essay.id}`}>
                 <div className='p-4 text-2xl hover:bg-red-700 hover:text-white rounded-md'>
                   <div className='font-regular'>
